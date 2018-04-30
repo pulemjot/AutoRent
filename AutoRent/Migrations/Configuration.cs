9d53ab09b450a8b17f55487bac27d617e3105709
pulemjot <anny.vasiljeva@gmail.com>
2018-04-30 12:57:14 +0300

    seed

--- AutoRent/Migrations/Configuration.cs
+++ AutoRent/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 using System.Security.Cryptography;
 using System.Text;
 using AutoRent.Database;
+using Microsoft.Win32.SafeHandles;
 
 namespace AutoRent.Migrations
 {
@@ -18,24 +19,63 @@ namespace AutoRent.Migrations
 
         protected override void Seed(AppDbContext context)
         {
-            string pwd = null;
-            using (var halg = HashAlgorithm.Create("sha256"))
+            if (!context.Operators.Any())
             {
-                var hash = halg.ComputeHash(Encoding.Unicode.GetBytes("password"));
-                foreach (var b in hash)
+                string pwd = null;
+                using (var halg = HashAlgorithm.Create("sha256"))
                 {
-                    pwd += $"{b:x2}";
+                    var hash = halg.ComputeHash(Encoding.Unicode.GetBytes("password"));
+                    foreach (var b in hash)
+                    {
+                        pwd += $"{b:x2}";
+                    }
                 }
+                var op = new Operator()
+                {
+                    UserName = "admin",
+                    HashedPassword = pwd
+                };
+                context.Operators.Add(op);
+                context.SaveChanges();
             }
-            if (context.Operators.Any()) { return; }
 
-            var op = new Operator()
+            if (!context.Cars.Any())
             {
-                UserName = "admin",
-                HashedPassword = pwd
-            };
-            context.Operators.Add(op);
-            context.SaveChanges();
+                var car = new CarEntity
+                {
+                    RegNumber = "ED-3124",
+                    GearUnitType = GearUnitType.Manual,
+                    Model = "RAV-4",
+                    Vendor = "Toyota",
+                    RentPricePerDay = 25,
+                    Year = 2001
+                };
+                context.Cars.Add(car);
+                context.SaveChanges();
+                car = new CarEntity
+                {
+                    RegNumber = "GH-3232",
+                    GearUnitType = GearUnitType.Automatic,
+                    Model = "RX",
+                    Vendor = "Lexus",
+                    RentPricePerDay = 35,
+                    Year = 2007
+                };
+                context.Cars.Add(car);
+                context.SaveChanges();
+                car = new CarEntity
+                {
+                    RegNumber = "HL-5544",
+                    GearUnitType = GearUnitType.Manual,
+                    Model = "XC90",
+                    Vendor = "Volvo",
+                    RentPricePerDay = 35,
+                    Year = 2010
+                };
+                context.Cars.Add(car);
+                context.SaveChanges();
+            }
+            
         }
     }
 }
