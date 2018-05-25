b7ee2d6f630d781f42096d871dd719358fd3375b
Vadims Podans <bokorezi@inbox.lv>
2018-05-25 11:08:39 +0300

    removed unnecessary LoginModel

--- AutoRent/Forms/Authorization.cs
+++ AutoRent/Forms/Authorization.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
 using AutoRent.Database;
-using AutoRent.Models;
 
 namespace AutoRent.Forms {
     public partial class Authorization : Form {
         const String LoginFailed = "The username or password is incorrect.";
         const String ConnectionFailed = "Could not establish connection with database.";
-        readonly LoginModel _loginModel = new LoginModel();
 
         public Authorization() {
             InitializeComponent();
-            loginBox.DataBindings.Add(nameof(TextBox.Text), _loginModel, nameof(LoginModel.UserName));
-            passwordBox.DataBindings.Add(nameof(TextBox.Text), _loginModel, nameof(LoginModel.Password));
         }
 
         void LoginButton_Click(Object sender, EventArgs e) {
             var mgr = new DbManager();
             try {
-                Boolean result = mgr.VerifyCredentials(_loginModel.UserName, _loginModel.Password);
+                Boolean result = mgr.VerifyCredentials(loginBox.Text, passwordBox.Text);
 
                 if (!result) {
                     InvalidLoginLabel.Text = LoginFailed;
